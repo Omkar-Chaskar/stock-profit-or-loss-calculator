0696571256e0288beb9a58c496887674b02fadb6
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2021-08-13 16:55:32 +0530

    update : NaN error occure

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-let purPrice = document.querySelector("#stock-purchase");
-let quantity = document.querySelector("#stock-quntity");
-let curPrice = document.querySelector("#stock-current");
-let btn = document.querySelector("#btn-submit")
+const purPrice = document.getElementById("stock-purchase");
+const quantity = document.getElementById("stock-quntity");
+const curPrice = document.getElementById("stock-current");
+const form = document.forms[0];
 let msg = document.querySelector("#msg")
 let inputSection = document.querySelector("input")
 
-btn.addEventListener("click", function(event){
-    event.preventDefault();
+
+form.addEventListener("submit",function clickHandler(e){
+    e.preventDefault();
+
+    let pP = Number(purPrice.value);
+    let qnt = Number(quantity.value);
+    let cP = Number(curPrice.value);
+    console.log(typeof purPrice.value , quantity.value ,curPrice.value)
     if(inputSection.value > 0){
-        purPrice = Number(purPrice.value);
-        quantity = Number(quantity.value);
-        curPrice = Number(curPrice.value);
         
-        if (purPrice > curPrice) {
+        
+        if(isNaN(purPrice) === true){
+            console.log("NaN value found")
+            
+        }
+        console.log(pP , qnt ,cP)
+        
+        if (pP > cP) {
             let loss = (purPrice - curPrice) * quantity;
-            let perProfit = ((purPrice - curPrice)/100)*purPrice ;
-            console.log(profit , perProfit ,"%")
-            msg.innerHTML ="You are in profit."
+            let perLoss = ((purPrice - curPrice)*100)/purPrice ;
+            console.log(typeof loss , perLoss ,"%")
+            msg.innerHTML ="You are in Loss."
         } else {
-            msg.innerHTML ="You are in loss."
+            let profit = (curPrice - purPrice) * quantity;
+            let perProfit = ((purPrice -curPrice)*100)/curPrice ;
+            console.log(profit , perProfit.toFixed(2) ,"%")
+            msg.innerHTML ="You are in profit."
         }
     }else{
         msg.innerHTML = "Please enter values greater than Zero."
